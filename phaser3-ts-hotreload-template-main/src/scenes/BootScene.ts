db5671a72664543dd395474e5b8a51de9d3459a7
Umut <unlu0413@gmail.com>
2021-08-23 18:38:33 +0300

    Revert "sssssss"
    This reverts commit 208ef708aacc8d852b0ed678978c94b691821171.

--- phaser3-ts-hotreload-template-main/src/scenes/BootScene.ts
+++ phaser3-ts-hotreload-template-main/src/scenes/BootScene.ts
@@ -21,15 +21,9 @@ export default class BootScene extends Phaser.Scene {
 		this.load.image("background", "assets/images/background.jpg");
 		this.load.image("player", "assets/images/player.png");
 		this.load.image("sword", "assets/images/sword.png");
-
-		//circle 
 		this.load.image("blue", "assets/images/blue.png");
-		this.load.image("red", "assets/images/red.png");
-		this.load.image("purple", "assets/images/purple.png");
-		//
 		this.load.image("eship", "assets/images/eship.png");
 
-		this.load.image("levelup", "assets/images/levelup.png");
 
 		this.load.atlas('knight', 'assets/images/knight.png', 'assets/images/knight.json');
 
@@ -61,10 +55,6 @@ export default class BootScene extends Phaser.Scene {
 		this.load.spritesheet('brawler', 'assets/images/brawler48x48.png', { 
 			frameWidth: 48,
 			frameHeight: 48 
-		});
-		this.load.spritesheet('sssword', 'assets/images/sssword.png', { 
-			frameWidth: 256,
-			frameHeight: 194 
 		});
 	}
 
--- phaser3-ts-hotreload-template-main/src/scenes/UIScene.ts
+++ phaser3-ts-hotreload-template-main/src/scenes/UIScene.ts
@@ -2,7 +2,7 @@ export default class UIScene extends Phaser.Scene {
 	gameScene: Phaser.Scene;
 	scoreText: Phaser.GameObjects.Text;
 	coinIcon: Phaser.GameObjects.Image;
-	levelIcon;
+
 	constructor() {
 		super("UI"); // Name of the scene
 	}
@@ -23,36 +23,11 @@ export default class UIScene extends Phaser.Scene {
 		});
 
 		this.coinIcon = this.add.image(15, 15, "items", 3);
-
-		//Buraya level atlanınca çıkacak olan spritesheet gelecek. 
-		this.levelIcon = this.physics.add.sprite(400, 100,'coin')
-		this.levelIcon.visible = false;
-		this.anims.create({
-            key: 'levelChange',
-            frames: this.anims.generateFrameNumbers('coin', { frames: [ 0, 1, 2, 3, 4 ] }),
-            frameRate: 8,
-            repeat: 4
-        });	
 	}
 
 	setupEvents(): void {
 		this.gameScene.events.on("updateScore", (score: number) => {
 			this.scoreText.setText(`Coins: ${score}`);
 		});
-		//Level atlanınca yapılcak olan işlemler burada çalışıyor.
-		this.gameScene.events.on("showLevel", () => {
-			this.levelIcon.visible = true;
-			this.levelIcon.play('levelChange');
-			let timedEvent = this.time.delayedCall(3000, this.invisibleEvent, [], this);
-		});
 	}
-
-	//Level atlandıktan 3 sn sonra levelIcon görünmez hale getirliyor.
-	invisibleEvent()
-	{
-		this.levelIcon.visible = false;
-	}
-
-
-
 }
